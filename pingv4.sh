b52220704a9458978bb5a7ce1f47fa7f750ec30d
omarcino <46736704+omarcino@users.noreply.github.com>
2021-06-21 18:06:41 -0400

    Update pingv4.sh

--- pingv4.sh
+++ pingv4.sh
@@ -8,6 +8,7 @@
 # ip address === x.x.x.x
 host="x.x.x.x"
 ipv="ipv4"
-#fn=$(date '+%Y%m%d')
-ping -O $host | while read pong; do echo "$(date '+%Y-%m-%d %H:%M:%S'): $pong"; done >> /root/ping/$(date '+%Y-%m-%d').$ipv-$host
+# directory path
+directory="/your/path/directory/"
+ping -O $host | while read pong; do echo "$(date '+%Y-%m-%d %H:%M:%S'): $pong"; done >> $directory/$(date '+%Y-%m-%d').$ipv-$host
   
